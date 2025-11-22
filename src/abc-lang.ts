6ad71e8accec59da85f675f74f2f56f5bcd4c4b8
LoopBraker <carrieta@udemedellin.edu.co>
2025-11-22 06:34:33 -0500

    abcjs-language v1.0.1

--- src/abc-lang.ts
+++ src/abc-lang.ts
@@ -6,9 +6,12 @@ export const abcLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       styleTags({
+        DirectiveKeyword: t.keyword,            // %%keyword - purple
+        InfoKey: t.variableName,                // T:, M:, K: - purple/blue
+        DirectiveArgs: t.string,                // arguments/values - green
         Comment: t.lineComment,                 // % comments - gray italic
+        InlineComment: t.lineComment,           // % at end of line - gray italic
         CommentedDirective: t.lineComment,      // %%% commented directives - gray italic
-        Directive: t.keyword,                   // %% directives - purple/magenta like keywords
       })
     ]
   }),
